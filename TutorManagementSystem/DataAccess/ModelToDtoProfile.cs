797fab2f3b3be163635b8e3539bc5d9b8bbf735e
Linh711 <linhnqhe153099@fpt.edu.vn>
2024-04-19 15:57:33 +0700

    fix get request from tutors

--- TutorManagementSystem/DataAccess/ModelToDtoProfile.cs
+++ TutorManagementSystem/DataAccess/ModelToDtoProfile.cs
@@ -26,7 +26,6 @@ namespace DataAccess
             CreateMap<Class, AddClassDto>();
             CreateMap<Request, RequestDto>()
                 .ForMember(dest => dest.ClassName, opt => opt.MapFrom(src => src.Class!.ClassName))
-                .ForMember(dest => dest.ParentName, opt => opt.MapFrom(src => src.Parent.Email))
                 .ForMember(dest => dest.SubjectName, opt => opt.MapFrom(src => src.Subject.SubjectName));
             CreateMap<Request, UpdateRequestDto>();
 
--- TutorManagementSystem/DataAccess/Repositories/RequestRepository.cs
+++ TutorManagementSystem/DataAccess/Repositories/RequestRepository.cs
@@ -17,28 +17,34 @@ namespace DataAccess.Repositories
         public IQueryable<RequestDto> GetRequestDetail(long id)
         {
             var query = from r in _context.Requests
-                        join sj in _context.Subjects on r.SubjectId equals sj.SubjectId
-                        join p in _context.People on r.ParentId equals p.PersonId
-                        join c in _context.Classes on r.ClassId equals c.ClassId
-                        join s in _context.People on r.StudentId equals s.PersonId
-                        join t in _context.People on r.TutorId equals t.PersonId
+                        join sj in _context.Subjects on r.SubjectId equals sj.SubjectId into sjJoin
+                        from sj in sjJoin.DefaultIfEmpty()
+                        join p in _context.People on r.ParentId equals p.PersonId into pJoin
+                        from p in pJoin.DefaultIfEmpty()
+                        join c in _context.Classes on r.ClassId equals c.ClassId into cJoin
+                        from c in cJoin.DefaultIfEmpty()
+                        join s in _context.People on r.StudentId equals s.PersonId into sJoin
+                        from s in sJoin.DefaultIfEmpty()
+                        join t in _context.People on r.TutorId equals t.PersonId into tJoin
+                        from t in tJoin.DefaultIfEmpty()
                         where r.RequestId == id
                         select new RequestDto
                         {
                             RequestId = r.RequestId,
-                            StudentName = s.FullName,
-                            TutorName = t.FullName,
-                            ParentName = p.FullName,
+                            StudentName = s != null ? s.FullName : "",
+                            TutorName = t != null ? t.FullName : "",
+                            ParentName = p != null ? p.FullName : "",
                             RequestType = r.RequestType,
-                            ClassName = c.ClassName,
+                            ClassName = c != null ? c.ClassName : "",
                             Level = r.Level,
-                            SubjectName = sj.SubjectName,
-                            SubjectId = sj.SubjectId,
+                            SubjectName = sj != null ? sj.SubjectName : "",
+                            SubjectId = sj != null ? sj.SubjectId : 0,
                             Price = r.Price,
                             CreatedAt = r.CreatedAt,
                             UpdatedAt = r.UpdatedAt,
                             Status = r.Status
                         };
+
             return query;
         }
 
@@ -122,24 +128,29 @@ namespace DataAccess.Repositories
         public IQueryable<RequestDto> SearchRequestsForTutor(long tutorId, string status, string requestType)
         {
             var query = from r in _context.Requests
-                        join sj in _context.Subjects on r.SubjectId equals sj.SubjectId
-                        join p in _context.People on r.ParentId equals p.PersonId
-                        join c in _context.Classes on r.ClassId equals c.ClassId
-                        join s in _context.People on r.StudentId equals s.PersonId
-                        join t in _context.People on r.TutorId equals t.PersonId
+                        join sj in _context.Subjects on r.SubjectId equals sj.SubjectId into sjJoin
+                        from sj in sjJoin.DefaultIfEmpty()
+                        join p in _context.People on r.ParentId equals p.PersonId into pJoin
+                        from p in pJoin.DefaultIfEmpty()
+                        join c in _context.Classes on r.ClassId equals c.ClassId into cJoin
+                        from c in cJoin.DefaultIfEmpty()
+                        join s in _context.People on r.StudentId equals s.PersonId into sJoin
+                        from s in sJoin.DefaultIfEmpty()
+                        join t in _context.People on r.TutorId equals t.PersonId into tJoin
+                        from t in tJoin.DefaultIfEmpty()
                         where r.TutorId == tutorId
                         && !r.Status!.Equals("CANCELLED")
                         select new RequestDto
                         {
                             RequestId = r.RequestId,
-                            StudentName = s.FullName,
-                            TutorName = t.FullName,
-                            ParentName = p.FullName,
+                            StudentName = s != null ? s.FullName : "",
+                            TutorName = t != null ? t.FullName : "",
+                            ParentName = p != null ? p.FullName : "",
                             RequestType = r.RequestType,
-                            ClassName = c.ClassName,
+                            ClassName = c != null ? c.ClassName : "",
                             Level = r.Level,
-                            SubjectName = sj.SubjectName,
-                            SubjectId = sj.SubjectId,
+                            SubjectName = sj != null ? sj.SubjectName : "",
+                            SubjectId = sj != null ? sj.SubjectId : 0,
                             Price = r.Price,
                             CreatedAt = r.CreatedAt,
                             UpdatedAt = r.UpdatedAt,
