413f5358addc1de286ecb4f651b2750954921f7f
DESKTOP-NITRMJO\ASUS <tungpthe140195@fpt.edu.vn>
2024-04-22 08:47:07 +0700

    up evaluation

--- TutorManagementSystem/DataAccess/Dtos/EvaluationDto.cs
+++ TutorManagementSystem/DataAccess/Dtos/EvaluationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,46 @@ namespace DataAccess.Dtos
 {
     public class EvaluationDto
     {
-        public long EvaluationId { get; set; }
+        public long? EvaluationId { get; set; }
         public long StudentId { get; set; }
         public long ClassId { get; set; }
+        public string? StudentName { get; set; }
+        public string? ClassName { get; set; }
         public int Score { get; set; }
         public string? Comment { get; set; }
         public DateTime? Date { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
+    }
+
+    public class AddEvaluationDto
+    {
+
+        public long StudentId { get; set; }
+        public long ClassId { get; set; }
+        public int Score { get; set; }
+        public string? Comment { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Status { get; set; } = null;
+    }
+
+    public class RequestEvaluationDto
+    {
+        [Range(0, long.MaxValue)]
+        public long ClassId { get; set; }
+        [Required]
+        public PagingRequest PagingRequest { get; set; } = null!;
+        public long? StudentId { get; set; }
+        public DateTime? Date { get; set; }
+    }
+
+    public class EvaluationForParentDto
+    {
+        [Range(0, long.MaxValue)]
+        public long StudentId { get; set; }
+        [Required]
+        public PagingRequest PagingRequest { get; set; } = null!;
+
     }
 }
---
+++ TutorManagementSystem/DataAccess/Repositories/EvaluationRepository.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Dtos;
+using DataAccess.Models;
+using DataAccess.Repositories.Interfaces;
+
+namespace DataAccess.Repositories
+{
+    public class EvaluationRepository : IEvaluationRepository
+    {
+        private readonly ClassNTutorContext _context;
+
+        public EvaluationRepository(ClassNTutorContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<EvaluationDto> GetDetailEvaluation(long evaluationId)
+        {
+            var query = from e in _context.Evaluations
+                        join c in _context.Classes on e.ClassId equals c.ClassId
+                        join p in _context.People on e.StudentId equals p.PersonId
+                        where e.EvaluationId == evaluationId
+                        select new EvaluationDto
+                        {
+                            EvaluationId = e.EvaluationId,
+                            StudentId = e.StudentId,
+                            StudentName = p.FullName,
+                            ClassId = e.ClassId,
+                            ClassName = c.ClassName,
+                            Score = e.Score,
+                            Comment = e.Comment,
+                            Date = e.Date,
+                            CreatedAt = e.CreatedAt,
+                            UpdatedAt = e.UpdatedAt,
+                            Status = e.Status
+                        };
+            return query;
+        }
+
+        public IQueryable<EvaluationDto> GetEvaluations(long classId, long? studentId, DateTime? date)
+        {
+            var query = from e in _context.Evaluations
+                        join c in _context.Classes on e.ClassId equals c.ClassId
+                        join p in _context.People on e.StudentId equals p.PersonId
+                        where e.ClassId == classId
+                        select new EvaluationDto
+                        {
+                            EvaluationId = e.EvaluationId,
+                            StudentId = e.StudentId,
+                            StudentName = p.FullName,
+                            ClassId = e.ClassId,
+                            ClassName = c.ClassName,
+                            Score = e.Score,
+                            Comment = e.Comment,
+                            Date = e.Date,
+                            CreatedAt = e.CreatedAt,
+                            UpdatedAt = e.UpdatedAt,
+                            Status = e.Status
+                        };
+
+            if (studentId == 0)
+            {
+                query = query.Where(c => c.StudentId! == studentId);
+            }
+            if (date.Equals(null))
+            {
+                query = query.Where(c => c.Date!.Equals(date));
+            }
+
+            return query.OrderBy(x => x.EvaluationId);
+        }
+
+
+    }
+}
+   
